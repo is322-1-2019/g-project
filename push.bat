2dc0989379eff1fdaf4e109ebaeebe0e959f69e2
pailin.h3 <jinjuta.l@st.econ.tu.ac.th>
2019-11-27 13:48:33 +0700

    Pailin push G project

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 git init
 git add .
-git commit -m "push i project"
-git remote add origin https://github.com/is322/i-project-pailin.git
+git commit -m "Pailin push G project"
+git remote add origin https://github.com/is322/g-project.git
 git push -u origin master
